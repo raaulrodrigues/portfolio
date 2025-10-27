7147718f7e17c92b7e7a6babb99de6b4311bd7fa
raaulrodriguees <clovisdavildo@gmail.com>
2025-10-27 12:35:03 -0300

    refactor: Usa variável de ambiente para Sanity Project ID

--- src/sanityClient.ts
+++ src/sanityClient.ts
@@ -1,5 +1,11 @@
 import { createClient } from '@sanity/client';
 
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID;
+
+if (!projectId) {
+  console.error("VITE_SANITY_PROJECT_ID não está definido! Verifique suas variáveis de ambiente.");
+}
+
 export default createClient({
   projectId: 'ed8h2w31', 
   dataset: 'production', 
